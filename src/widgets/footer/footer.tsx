264aacfa667dfc0aa025c65b99076a0d2e20d056
MaksimNikulshin <122968824+MaksimNikulshin@users.noreply.github.com>
2024-02-06 20:10:39 +0300

    fix

--- src/widgets/footer/footer.tsx
+++ src/widgets/footer/footer.tsx
@@ -46,23 +46,23 @@ const Footer: React.FC = () => {
       </div> */}
       <div className='footer__about'>
         <p className='footer__title'>About</p>
-        <p>Home</p>
-        <p>Marketplace</p>
-        <p>Creators</p>
-        <p>Community</p>
-        <p>FAQ</p>
+        <p className='footer__link'>Home</p>
+        <p className='footer__link'>Marketplace</p>
+        <p className='footer__link'>Creators</p>
+        <p className='footer__link'>Community</p>
+        <p className='footer__link'>FAQ</p>
       </div>
       <div className='footer__company'>
         <p className='footer__title'>Company</p>
-        <p>Our Team</p>
-        <p>Partner With Us</p>
-        <p>Privacy & Policy</p>
-        <p>Features</p>
+        <p className='footer__link'>Our Team</p>
+        <p className='footer__link'>Partner With Us</p>
+        <p className='footer__link'>Privacy & Policy</p>
+        <p className='footer__link'>Features</p>
       </div>
       <div className='footer__contact'>
         <p className='footer__title'>Contact</p>
-        <p>+7 999 000 01 23</p>
-        <p>w3bz@company.com</p>
+        <p className='footer__link'>+7 999 000 01 23</p>
+        <p className='footer__link'>w3bz@company.com</p>
       </div>
     </footer>
   )
