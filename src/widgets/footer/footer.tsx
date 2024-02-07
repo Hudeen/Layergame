eab09da89c63e2a9aa83f68bdaf12eec637b9a81
MaksimNikulshin <122968824+MaksimNikulshin@users.noreply.github.com>
2024-02-07 15:24:10 +0300

    footer

--- src/widgets/footer/footer.tsx
+++ src/widgets/footer/footer.tsx
@@ -1,68 +1,67 @@
-//Import components
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-
 //Import styles
-import './footer.css'
+import './footer.scss'
 
 //Import images
 import logo from '../../shared/assets/icons/logo.svg'
-// import twitter from '../../shared/assets/icons/twitter.png'
-// import facebook from '../../shared/assets/icons/facebook.png'
-// import linked from '../../shared/assets/icons/linked.png'
-// import telegram from '../../shared/assets/icons/telegram.png'
+import twitter from '../../shared/assets/icons/twitter.svg'
+import yuoTube from '../../shared/assets/icons/yuoTube.svg'
+import facebook from '../../shared/assets/icons/facebook.svg'
+import linked from '../../shared/assets/icons/linked.svg'
+import telegram from '../../shared/assets/icons/telegram.svg'
 
 const Footer: React.FC = () => {
-  gsap.registerPlugin(ScrollTrigger)
-
   return (
     <footer>
-      <div className='footer__main'>
-        <div className='footer__logo'>
+      <div className='main'>
+        <div className='logo'>
           <img
             src={logo}
             alt='logo'
           />
-          <h1 className='header__title'>LayerGame</h1>
+          <h1>LayerGame</h1>
+        </div>
+        <div className='social'>
+          <img
+            src={twitter}
+            alt='twitter'
+          />
+          <img
+            src={yuoTube}
+            alt='yuoTube'
+          />
+          <img
+            src={facebook}
+            alt='facebook'
+          />
+          <img
+            src={linked}
+            alt='linked'
+          />
+          <img
+            src={telegram}
+            alt='telegram'
+          />
         </div>
       </div>
-      {/* <div className='footer__social'>
-        <img
-          src={twitter}
-          alt='twitter'
-        />
-        <img
-          src={facebook}
-          alt='facebook'
-        />
-        <img
-          src={linked}
-          alt='linked'
-        />
-        <img
-          src={telegram}
-          alt='telegram'
-        />
-      </div> */}
-      <div className='footer__about'>
-        <p className='footer__title'>About</p>
-        <p className='footer__link'>Home</p>
-        <p className='footer__link'>Marketplace</p>
-        <p className='footer__link'>Creators</p>
-        <p className='footer__link'>Community</p>
-        <p className='footer__link'>FAQ</p>
+      <div className='links'>
+        <p>About</p>
+        <p>Home</p>
+        <p>Marketplace</p>
+        <p>Creators</p>
+        <p>Community</p>
+        <p>FAQ</p>
       </div>
-      <div className='footer__company'>
-        <p className='footer__title'>Company</p>
-        <p className='footer__link'>Our Team</p>
-        <p className='footer__link'>Partner With Us</p>
-        <p className='footer__link'>Privacy & Policy</p>
-        <p className='footer__link'>Features</p>
+      <div className='links'>
+        <p>Company</p>
+        <p>Our Team</p>
+        <p>Partner With Us</p>
+        <p>Privacy & Policy</p>
+        <p>Features</p>
       </div>
-      <div className='footer__contact'>
-        <p className='footer__title'>Contact</p>
-        <p className='footer__link'>+7 999 000 01 23</p>
-        <p className='footer__link'>w3bz@company.com</p>
+      <div className='links'>
+        <p>Contact</p>
+        <p>+7 999 000 01 23</p>
+        <p>w3bz@company.com</p>
       </div>
     </footer>
   )
