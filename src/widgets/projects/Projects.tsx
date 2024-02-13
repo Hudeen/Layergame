bd5d5306a761f3260ff1fa6be3cc15a91c34228f
MaksimNikulshin <122968824+MaksimNikulshin@users.noreply.github.com>
2024-02-13 19:58:03 +0300

    responsive

--- src/widgets/projects/Projects.tsx
+++ src/widgets/projects/Projects.tsx
@@ -1,49 +1,52 @@
-import "./Projects.css";
-import { gsap } from 'gsap';
-import { useGSAP } from "@gsap/react";
-import ProjectCard from "./projectCard/ProjectCard";
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import './Projects.scss'
+import { gsap } from 'gsap'
+import { useGSAP } from '@gsap/react'
+import ProjectCard from './projectCard/ProjectCard'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { useRef } from 'react'
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger)
 
 const Projects = () => {
+  const containerRef = useRef<HTMLDivElement>(null)
+
   useGSAP(() => {
-    gsap.to('.card__container', {
-      scrollTrigger: {
-        trigger: '.card__container',
-        start: 'top center-=220',
-        end: 'bottom+=800 center',
-        scrub: true,
-        pin: ".wrapper",
-        toggleActions: 'restart none none reverse',
-        pinType: "transform"
-      },
-      x: -2500
-    });
+    if (containerRef.current && containerRef.current.offsetWidth >= 1280) {
+      gsap.to('.card__container', {
+        scrollTrigger: {
+          trigger: '.card__container',
+          start: 'top center-=220',
+          end: 'bottom+=800 center',
+          scrub: true,
+          pin: '.wrapper',
+          toggleActions: 'restart none none reverse',
+          pinType: 'transform',
+        },
+        x: -4200,
+      })
 
-    const progressValue = document.querySelector('.progress-value');
-    gsap.to(progressValue, {
-      scrollTrigger: {
-        trigger: '.card__container',
-        start: 'top center-=200',
-        end: 'bottom+=800 center',
-        scrub: 1,
-      },
-      width: "100%",
-    });
-
-
-  }, []);
+      const progressValue = document.querySelector('.progress-value')
+      gsap.to(progressValue, {
+        scrollTrigger: {
+          trigger: '.card__container',
+          start: 'top center -=200',
+          end: 'bottom+=800 center',
+          scrub: 1,
+        },
+        width: '100%',
+      })
+    }
+  }, [])
 
   return (
-    <div className="wrapper">
-      <div className="progress">
-        <div className="progress-value"></div>
+    <div
+      className='wrapper'
+      ref={containerRef}>
+      <div className='progress'>
+        <div className='progress-value'></div>
       </div>
-      <p className="title">
-        Team Project
-      </p>
-      <div className="card__container">
+      <p className='title'>the project team</p>
+      <div className='card__container'>
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
@@ -52,7 +55,7 @@ const Projects = () => {
         <ProjectCard />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Projects;
+export default Projects
