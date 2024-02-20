e128318e8fef5e821a9138e9c8e25c6fa9e0a518
Chepman32 <antonkerch555@gmail.com>
2024-02-20 10:47:32 +0300

    Mint scss

--- src/pages/Mint/index.tsx
+++ src/pages/Mint/index.tsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react"
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { useGSAP } from '@gsap/react'
-import "./styles.css"
+import "./styles.scss"
 import Planet from "../../shared/assets/images/planet.png"
 import NFT from "../../shared/assets/images/imgNFT.png"
 
