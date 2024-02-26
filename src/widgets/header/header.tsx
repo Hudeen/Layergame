c4509d3562b648e7477096817f90400bd7668b24
Hudeen <klaud.i@mail.ru>
2024-02-26 04:35:03 +0300

    optimized header

--- src/widgets/header/header.tsx
+++ src/widgets/header/header.tsx
@@ -1,8 +1,7 @@
 import React, { useRef, useState } from 'react'
-import './header.css'
 import './header.scss'
 import { gsap } from 'gsap'
-import { useGSAP } from '@gsap/react'
+
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import logo from '../../shared/assets/icons/logo.svg'
 
@@ -36,27 +35,27 @@ const Header: React.FC = () => {
     setIsAnimating(false)
   }
 
-  useGSAP(() => {
-    const options = {
-      trigger: containerRef.current,
-      start: '-35px',
-      toggleActions: 'restart none none reverse',
-    }
+  // useGSAP(() => {
+  //   const options = {
+  //     trigger: containerRef.current,
+  //     start: '-35px',
+  //     toggleActions: 'restart none none reverse',
+  //   }
 
-    gsap.from(containerRef.current, {
-      x: -40,
-      opacity: 0,
-      duration: 0.4,
-      scrollTrigger: options,
-    })
+  //   gsap.from(containerRef.current, {
+  //     x: -40,
+  //     opacity: 0,
+  //     duration: 0.4,
+  //     scrollTrigger: options,
+  //   })
 
-    gsap.to(containerRef.current, {
-      x: 0,
-      opacity: 1,
-      duration: 0.4,
-      scrollTrigger: options,
-    })
-  }, [])
+  //   gsap.to(containerRef.current, {
+  //     x: 0,
+  //     opacity: 1,
+  //     duration: 0.4,
+  //     scrollTrigger: options,
+  //   })
+  // }, [])
 
   return (
     <header>
@@ -65,7 +64,8 @@ const Header: React.FC = () => {
         ref={navigationRef}
         onMouseLeave={handleMouseLeave}
         onAnimationStart={handleAnimationStart}
-        onAnimationEnd={handleAnimationEnd}>
+        onAnimationEnd={handleAnimationEnd}
+        >
         <h1 className='title'>Platea vitae interdum.</h1>
         <h2 className='subtitle'>Dui cras et. Malesuada eget odio. Pellentesque orci.</h2>
         {/* <div className='item'>
@@ -73,7 +73,7 @@ const Header: React.FC = () => {
           <a className='link'>Link2</a>
           <a className='link'>Link3</a>
         </div> */}
-        <div className='glare'></div>
+        {/* <div className='glare'></div> */}
       </div>
 
       <div className='header__item'>
@@ -160,6 +160,7 @@ const Header: React.FC = () => {
                 d='M54 1H3.41422L23.1357 20.7215C25.4956 23.0814 25.4762 26.9134 23.0926 29.2494L2.93892 49H54C56.2091 49 58 47.2091 58 45V5C58 2.79086 56.2091 1 54 1Z'
                 stroke='url(#paint0_linear_96_16)'
                 stroke-width='2'
+                fill='#000'
               />
               <path
                 d='M47 31H31C30.45 31 30 30.55 30 30C30 29.45 30.45 29 31 29H47C47.55 29 48 29.45 48 30C48 30.55 47.55 31 47 31ZM47 26H31C30.45 26 30 25.55 30 25C30 24.45 30.45 24 31 24H47C47.55 24 48 24.45 48 25C48 25.55 47.55 26 47 26ZM48 20C48 20.55 47.55 21 47 21H31C30.45 21 30 20.55 30 20C30 19.45 30.45 19 31 19H47C47.55 19 48 19.45 48 20Z'
